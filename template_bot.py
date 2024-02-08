491f83a7970c57283a8dfa73fac5ab859627f343
Mansur Nurmukhambetov <56600275+nomomon@users.noreply.github.com>
2024-02-08 14:15:16 +0100

    Update template_bot.py

--- template_bot.py
+++ template_bot.py
@@ -1,2 +1,24 @@
-if __name__ == "__main__":
-  print("Meow world!")
+import chess
+import random
+
+from abc import ABC, abstractmethod
+
+
+class ChessBotClass(ABC):
+    @abstractmethod
+    def __call__(self, board_fen: str) -> chess.Move:
+        pass
+
+
+# keep the bot named ChessBot when submitting
+class ChessBot(ChessBotClass):
+    def __call__(self, board_fen):
+        board = chess.Board(board_fen)
+
+        moves = list(board.legal_moves)
+
+        idx = int(random.random() * len(moves))
+
+        move = moves[idx]
+
+        return move
